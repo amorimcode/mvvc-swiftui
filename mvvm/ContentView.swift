a30072c7a02f2449f27b2250d912df6b5a7436af
Bruno Amorim <bruno.amorim@mblabs.com.br>
2023-10-02 22:41:35 -0300

    Initial Commit

---
+++ mvvm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mvvm
+//
+//  Created by Bruno Amorim on 02/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mvvm/mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmApp.swift
+//  mvvm
+//
+//  Created by Bruno Amorim on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
