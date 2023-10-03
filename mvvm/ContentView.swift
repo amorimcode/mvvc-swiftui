3c6c32bf6d65e0c87161bafdf7c37d2958cf85ca
amorimcode <bruno.amorim032@gmail.com>
2023-10-02 23:27:51 -0300

    mvvm

--- mvvm/ContentView.swift
+++ mvvm/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var model: ContentViewModel
+        
+    init(model: ContentViewModel) {
+        self.model = model
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(model.state.isLoading ? "Carregando..." : model.state.message)
+                .onAppear(perform: model.loadData)
         }
         .padding()
     }
 }
 
+struct ContentViewState {
+      var isLoading = false
+      var message = ""
+}
+
+class ContentViewModel: ObservableObject {
+    private(set) var state: ContentViewState
+    
+    
+    init(initialState: ContentViewState = .init()) {
+        state = initialState
+    }
+    
+    func loadData() {
+        state.isLoading = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
+            self.state.isLoading = false
+            self.state.message = "Olá mundo"
+        }
+    }
+}
+
 #Preview {
-    ContentView()
+    ContentView(model: .init())
 }
